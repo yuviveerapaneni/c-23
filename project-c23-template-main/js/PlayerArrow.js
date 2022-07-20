f97153d89629fc075c9b165d1d0ba6d846ea9184
yuviveerapaneni <87616916+yuviveerapaneni@users.noreply.github.com>
2022-07-20 18:53:26 -0500

    Add files via upload

---
+++ project-c23-template-main/js/PlayerArrow.js
@@ -0,0 +1,20 @@
+class PlayerArrow {
+  constructor(x, y, width, height, archerAngle) {
+    var options = {
+      isStatic: true,
+      density: 0.1
+    };
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    this.image = loadImage("./assets/arrow.png");
+    this.archerAngle = archerAngle;
+    this.velocity = 0;
+    World.add(world, this.body);
+  }
+
+  display() {
+  
+   
+  }
+}
